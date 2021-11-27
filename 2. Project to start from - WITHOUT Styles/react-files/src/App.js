68865e32489636f7cc4d18b42e938e12ae1347ef
federicoprat <82243049+federicoprat@users.noreply.github.com>
2021-11-27 15:47:13 -0300

    estilos completados, video en 1:09:00

---
+++ 2. Project to start from - WITHOUT Styles/react-files/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//styles
+import {GlobalStyle} from './GlobalStyle';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Start here.
+      <GlobalStyle /> 
+    </div>
+  );
+}
+
+export default App;
