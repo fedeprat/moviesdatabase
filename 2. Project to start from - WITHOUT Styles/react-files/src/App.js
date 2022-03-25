8c6407b3634bee8deda89e7eb7fccbb606b5812a
federicoprat <82243049+federicoprat@users.noreply.github.com>
2022-03-25 16:54:58 -0300

    Update App.js

--- 2. Project to start from - WITHOUT Styles/react-files/src/App.js
+++ 2. Project to start from - WITHOUT Styles/react-files/src/App.js
@@ -12,7 +12,15 @@ import NotFound from './components/NotFound'
 
 
 const App = () => (
-    <div>BASURAAAAAAAA</div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path = "/" element = {<Home/>} />
+        <Route path = '/:movieId' element = {<Movie/>} />
+        <Route path = '/*' element = {<NotFound/>} />
+      </Routes>
+      <GlobalStyle /> 
+    </Router>
   );
  
 
